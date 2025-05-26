55f0218c591c4a1d2fa9527787afeb83102014d4
Legion-ke <omondigiven@gmail.com>
2025-05-26 23:38:55 +0300

    frontend fix errors again

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 export default {
-  kit: {
-    adapter: adapter({
-      fallback: '200.html' // For SPA routing
-    })
-  },
-  preprocess: vitePreprocess()
+	kit: {
+		adapter: adapter({
+			fallback: '200.html' // For SPA routing
+		})
+	},
+	preprocess: vitePreprocess()
 };
